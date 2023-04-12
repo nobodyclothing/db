9485b6483497b9d12285568e5b7abb0a57fd72c0
mczernyk <mczernyk@gmail.com>
2023-04-11 23:42:06 -0400

    removed custom theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider, connectorsForWallets, Theme
+import { getDefaultWallets, RainbowKitProvider, connectorsForWallets
 } from '@rainbow-me/rainbowkit';
 import type { AppProps } from 'next/app';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
@@ -33,46 +33,11 @@ const wagmiClient = createClient({
   webSocketProvider,
 });
 
-// const myCustomTheme: Theme = {
-//   colors: {
-//     accentColor: '#c3c3c3',
-//     accentColorForeground: 'black',
-//     actionButtonBorder: 'grey',
-//     actionButtonBorderMobile: '#c3c3c3',
-//     actionButtonSecondaryBackground: '#c3c3c3',
-//     closeButton: 'black',
-//     closeButtonBackground: 'transparent',
-//     connectButtonBackground: '#c3c3c3',
-//     connectButtonBackgroundError: '#c3c3c3',
-//     connectButtonInnerBackground: '#c3c3c3',
-//     connectButtonText: 'black',
-//     connectButtonTextError: 'black',
-//     connectionIndicator: '#c3c3c3',
-//     downloadBottomCardBackground: '#c3c3c3',
-//     downloadTopCardBackground: '#c3c3c3',
-//     error: '#c3c3c3',
-//     generalBorder: 'transparent',
-//     generalBorderDim: 'transparent',
-//     menuItemBackground: '#c3c3c3',
-//     modalBackdrop: 'transparent',
-//     modalBackground: '#c3c3c3',
-//     modalBorder: 'black',
-//     modalText: 'black',
-//     modalTextDim: 'black',
-//     modalTextSecondary: 'black',
-//     profileAction: '#c3c3c3',
-//     profileActionHover: '#c3c3c3',
-//     profileForeground: '#c3c3c3',
-//     selectedOptionBorder: 'grey',
-//     standby: '#c3c3c3',
-//   },
-// }
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider
-      // theme={myCustomTheme}
       chains={chains}
       appInfo={demoAppInfo} >
         <Component {...pageProps} />
